71cb916587225cddbc147fdadf632cd0e2214b82
Denis <Savchenkodkz@gmail.com>
2022-11-23 22:55:12 +0600

    Задача 2.1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = {2, 3, 7};
+int max = numbers[0];
+int index = 0;
+int size = 3;
+while (index < size) {
+    if (numbers[index] > max) {
+        max = numbers[index];
+        index ++;
+    }
+    else {
+        index ++;
+    }
+}
+Console.Write(max);
