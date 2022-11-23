a9e127f36e406efdfd072c4e7e1d1577f8067a45
Denis <Savchenkodkz@gmail.com>
2022-11-23 22:47:49 +0600

    Задача 1.1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 7;
+int max = 0;
+if (a > b) {
+    max = a;
+}
+else {
+    max = b;
+}
+Console.Write(max);
